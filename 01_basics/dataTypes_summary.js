3c847beffc3a7001ef56ec270fcd0e1a53a80275
Pramit R. <121679713+pramitbd@users.noreply.github.com>
2024-02-07 12:15:49 +0000

    stack heap

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -33,3 +33,32 @@ const myFunction = function () {
 console.log(typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "pramit@gmail.com";
+
+let anotherName = myYoutubename;
+
+anotherName = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "pramit@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);

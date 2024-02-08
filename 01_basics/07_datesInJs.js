6f4e5e5945ee8fdb30d5ba620eb220c3c021b7e5
Pramit R. <121679713+pramitbd@users.noreply.github.com>
2024-02-08 04:37:48 +0000

    date and time updated

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -16,7 +16,17 @@ let myCreatedDate = new Date("02-8-2024");
 
 let myTimeStamp = Date.now();
 
-console.log(myTimeStamp);
-console.log(myTimeStamp);
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
 
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
 
+// `${newDate.getDate()} and the time `
+
+newDate.toLocaleString("default", {
+    weekday: "long",
+});

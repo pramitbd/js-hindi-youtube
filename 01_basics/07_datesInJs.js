25bcffc1e32c7899e8edb9e3b9016d4ede03b28f
Pramit R. <121679713+pramitbd@users.noreply.github.com>
2024-02-08 04:25:06 +0000

    date and time

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,22 @@
+// Dates
+
+let myDate = new Date();
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024, 1, 8);
+// let myCreatedDate = new Date(2024, 1, 8, 5, 3);
+// let myCreatedDate = new Date("2024-02-8");
+let myCreatedDate = new Date("02-8-2024");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+console.log(myTimeStamp);
+console.log(myTimeStamp);
+
+

ced74d1ae776eaaf47fe341cf021419e00fe5528
Pramit R. <121679713+pramitbd@users.noreply.github.com>
2024-02-09 08:05:21 +0000

    objects updated

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -16,10 +16,26 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"],
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
-console.log(JsUser[mySym]);
-console.log(typeof mySym);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+// console.log(typeof mySym);
 
-    
+JsUser.email = "pramit@chatgpt.com";
+// Object.freeze(JsUser);
+JsUser.email = "pramit@microsoft.com"
+
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log(("Hello JS user"));
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());

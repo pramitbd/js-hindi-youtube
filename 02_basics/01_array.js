07de3cb5599a86e5aa17b13b1027c8d06dc6b109
Pramit R. <121679713+pramitbd@users.noreply.github.com>
2024-02-09 01:28:13 +0000

    array updated

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -4,7 +4,7 @@ const myArr = [0, 1, 2, 3, 4, 5];
 const myHeros = ["batman", "superman", "spiderman"];
 
 const myArr2 = new Array(1, 2, 3, 4);
-console.log(myArr[1]);
+// console.log(myArr[1]);
 
 // Array methods
 
@@ -13,10 +13,10 @@ console.log(myArr[1]);
 // myArr.pop();
 
 // myArr.unshift(9);
-myArr.shift();
+// myArr.shift();
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
 
 // const newArr = myArr.join();
 
@@ -25,3 +25,13 @@ console.log(myArr.indexOf(3));
 
 // slice, splice
 
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1, 3);
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log("C ", myArr);
+console.log(myn2);

0e248bea229236511182d2b180303a500cbf3ef4
Pramit R. <121679713+pramitbd@users.noreply.github.com>
2024-02-11 09:33:46 +0000

    scopes updated

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,9 +5,56 @@ if (true){
     let a = 10;
     const b = 20;
     // var c = 30;
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one(){
+    const username = "pramit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+
+}
+
+// one()
+
+
+if (true) {
+    const username = "Pramit"
+    if (username === "Pramit") {
+        const website = " YouTube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++ interesting +++++++++++++++++++
+
+
+function addOne(num) {
+    return num + 1
+
+}
+
+addone(5)
+
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+addTwo(5)

e9bb257764a58b867aa346e3e45a9ec06554eb15
Pramit R. <121679713+pramitbd@users.noreply.github.com>
2024-02-13 10:19:08 +0000

    control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,2 +1,24 @@
 // if
 
+const isUserLoggedIn = true;
+const temperature = 41;
+
+// if (temperature === 40) {
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50")
+// }
+
+// console.log("Execute");
+
+// <, >, <=, >=, ==, !=, ===, !== 
+
+// const score = 200;
+
+// if (score > 100) {
+//     let power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log((`User power: ${power}`));
+
